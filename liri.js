efa5c071a385fc6f1d81d329f5305e4ade058ba6
acarrillo2019 <alric21@aol.com>
2020-02-12 08:02:09 -0700

    Added required NPM

--- liri.js
+++ liri.js
@@ -1,5 +1,9 @@
+// Required NPM
+
 require("dotenv").config();
-
 var keys = require("./keys.js");
-
+var Spotify = require('node-spotify-api');
 var spotify = new Spotify(keys.spotify);
+var axios = require('axios');
+var moment = require('moment');
+var fs = require('fs');
